ddb2d162b885e720026cb5b9b9e1d3d0e1b59858
Jeslor Ssozi <jbestius@gmail.com>
2025-04-20 19:05:17 +0200

    add the account starter code

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
+import Account from "@/components/Account";
 import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <h1>Welcome to Jeslor Voice Assistant</h1>
-      <p>
-        This is a simple web application that allows you to create interviews
-        with Jeslor Voice Assistant.
-      </p>
-      <p>
-        You can use the app to generate questions and answers for your
-        interviews, and even record your own voice.
-      </p>
-      <p>
-        The app is built using Next.js, TypeScript, and Tailwind CSS. It is
-        designed to be fast, responsive, and easy to use.
-      </p>
-    </div>
-  );
+  return <Account />;
 };
 
 export default page;
