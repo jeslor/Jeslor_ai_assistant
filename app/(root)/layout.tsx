56027f070f0e0d5e42d5e9174b1d48e1f9c1491e
Jeslor Ssozi <jbestius@gmail.com>
2025-04-21 16:50:31 +0200

    add the root group

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-dark1/95 via-black-900 to-dark1">
+      {children}
+      <div className="absolute -top-40 -right-10 w-32 h-32 bg-primary1/45 rounded-full filter blur-3xl opacity-40 animate-spin-slow"></div>
+      <div className="absolute -bottom-10 -left-10 w-24 h-24 bg-secondary1 rounded-full filter blur-2xl opacity-30 animate-pulse"></div>
+    </div>
+  );
+};
+
+export default RootLayout;
