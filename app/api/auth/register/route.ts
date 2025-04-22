a8f3b17096baba4dfe1137bcfd009b161dd1863e
Jeslor Ssozi <jbestius@gmail.com>
2025-04-22 12:01:14 +0200

    add the register route

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,48 @@
+import prisma, { connectToDatabase } from "@/lib/prisma/prisma";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+import { accountValidator } from "@/lib/validators/account.validator";
+
+export const POST = async (req: Request) => {
+  const body = await req.json();
+  const { email, password, username } = accountValidator.parse(body);
+
+  try {
+    await connectToDatabase();
+    const userExists = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (userExists) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 409 }
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+        username,
+      },
+    });
+    return NextResponse.json(user, { status: 201 });
+  } catch (error: any) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ error: error.errors }, { status: 422 });
+    }
+    if (error.code === "P2002") {
+      return NextResponse.json(
+        { error: "Email already exists" },
+        { status: 409 }
+      );
+    }
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
--- lib/prisma/prisma.ts
+++ lib/prisma/prisma.ts
@@ -1,15 +1,41 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
 declare global {
   var prisma: PrismaClient | undefined;
+  var isPrismaConnected: boolean | undefined;
 }
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+
+const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+  global.isPrismaConnected = global.isPrismaConnected ?? false;
 }
+
 export default prisma;
+
+export const connectToDatabase = async () => {
+  try {
+    if (!global.isPrismaConnected) {
+      await prisma.$connect();
+      global.isPrismaConnected = true;
+      console.log("✅ Connected to the database");
+    } else {
+      console.log("ℹ️ Prisma is already connected");
+    }
+  } catch (error) {
+    console.error("❌ Error connecting to the database:", error);
+  }
+};
+
+export const disconnectFromDatabase = async () => {
+  try {
+    if (global.isPrismaConnected) {
+      await prisma.$disconnect();
+      global.isPrismaConnected = false;
+      console.log("🔌 Disconnected from the database");
+    }
+  } catch (error) {
+    console.error("❌ Error disconnecting from the database:", error);
+  }
+};
