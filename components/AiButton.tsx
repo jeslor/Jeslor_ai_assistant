cbeefde79a679789aebe9ac7d7e04427feda4ece
Jeslor Ssozi <jbestius@gmail.com>
2025-05-07 16:11:39 +0200

    fix colors

--- components/AiButton.tsx
+++ components/AiButton.tsx
@@ -12,7 +12,7 @@ const AiButton = ({ onPress, title, icon, extraClasses }: ButtonProps) => {
   return (
     <button
       onClick={() => onPress()}
-      className={`flex items-center gap-2 px-4 py-1.5 rounded-full bg-gradient-to-bl from-dark/30 to-primary1/10 backdrop-blur-md text-white shadow-inner  border border-white/10 text-sm hover:bg-gradient-to-bl hover:from-dark/50 hover:to-primary1/50 transition duration-300 ease-in-out cursor-pointer ${extraClasses}`}
+      className={`flex items-center gap-2 px-4 py-1.5 rounded-full bg-gradient-to-bl from-gray-900 to-primary1/10 backdrop-blur-md text-white shadow-inner  border border-white/10 text-sm hover:bg-gradient-to-bl hover:from-dark/50 hover:to-primary1/50 transition duration-300 ease-in-out cursor-pointer ${extraClasses}`}
     >
       <span className="text-[13px] font-medium flex items-center gap-x-2">
         {title}
