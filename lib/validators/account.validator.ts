18ca54d5556fa19c4281ed503f38aa2a1d57063d
Jeslor Ssozi <jbestius@gmail.com>
2025-05-11 13:44:08 +0300

    add all special characters

--- lib/validators/account.validator.ts
+++ lib/validators/account.validator.ts
@@ -9,5 +9,8 @@ export const accountValidator = z.object({
     .max(20, "Password must be at most 20 characters long")
     .regex(/[a-zA-Z]/, "Password must contain at least one letter")
     .regex(/[0-9]/, "Password must contain at least one number")
-    .regex(/[@$!%*?&]/, "Password must contain at least one special character"),
+    .regex(
+      /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+      "Password must contain at least one special character"
+    ),
 });
