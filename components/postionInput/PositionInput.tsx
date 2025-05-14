f9b0f9a059773c47d2a5e053f580ec3de1b6ae4b
Jeslor Ssozi <jbestius@gmail.com>
2025-05-14 14:41:09 +0300

    add the functionality to paste a job description

--- components/postionInput/PositionInput.tsx
+++ components/postionInput/PositionInput.tsx
@@ -2,19 +2,47 @@
 
 import { Icon } from "@iconify/react/dist/iconify.js";
 import AiButton from "../AiButton";
+import { useState } from "react";
 
 const PositionInput = () => {
+  const [positionInput, setPositionInput] = useState("");
+
+  const handlePaste = async () => {
+    try {
+      const text = await navigator.clipboard.readText();
+      setPositionInput(text);
+    } catch (err) {
+      setPositionInput("Could not read clipboard!");
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-y-2 ">
       <div className="card-wrapper w-full max-w-[800px] mx-auto mt-5 ">
         <div className="flex  gap-x items-center justify-around flex-1  rounded-4xl bg-black min-h-[322px]  backdrop-blur-md  px-4 py-2 shadow-xl border border-dark1/10 mx-auto relative w-[calc(100%-5px)] h-[90%] p-4">
-          <div className="absolute z-[2] h-full w-full p-4 text-[14px] opacity-80">
-            <textarea className="h-full w-full outline-0" />
+          <div
+            className={`absolute ${
+              positionInput.length > 0 ? "z-[2]" : "z-[1]"
+            } h-full w-full p-4 text-[14px] opacity-80`}
+          >
+            <textarea
+              value={positionInput}
+              onChange={(e) => setPositionInput(e.target.value)}
+              placeholder="Paste job position here..."
+              className="h-full w-full outline-0"
+            />
           </div>
-          <div className="flex items-center justify-center gap-1 opacity-75 absolute top-2 left-2 h-full w-full">
+          <button
+            onClick={handlePaste}
+            className={`${
+              positionInput.length === 0
+                ? "z-[2] opacity-75"
+                : "z-[1] opacity-0"
+            } flex items-center justify-center gap-1  absolute top-2 left-2 h-full w-full cursor-pointer`}
+          >
             <Icon icon="mingcute:paste-line" className="size-10" />
             Paste
-          </div>
+          </button>
         </div>
       </div>
       <AiButton
