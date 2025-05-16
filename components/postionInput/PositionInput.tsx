a7cf6d284f728f627d82aa0c105a9a098c6dd2c6
Jeslor Ssozi <jbestius@gmail.com>
2025-05-16 13:34:41 +0300

    start on the form validator on position Input

--- components/postionInput/PositionInput.tsx
+++ components/postionInput/PositionInput.tsx
@@ -4,6 +4,14 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 import AiButton from "../AiButton";
 import { useEffect, useState } from "react";
 import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
   Select,
   SelectContent,
   SelectGroup,
@@ -17,10 +25,20 @@ import { toast } from "sonner";
 import useUserStore from "../provider/userStore";
 import { createInterview } from "@/lib/actions/interview.actions";
 import useInterviewStore from "../provider/interViewStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { interviewValidator } from "@/lib/validators/interviewValidator";
 
 interface PositionInputProps {
   positionInput: string;
   setPositionInput: (input: string) => void;
+}
+
+interface AccountFormData {
+  companyWebsite: string;
+  totalQuestions: number;
+  type: "behavioral" | "technical" | "mixed";
+  level: "junior" | "mid" | "senior" | "lead" | "intern";
 }
 
 const PositionInput = ({
@@ -30,6 +48,15 @@ const PositionInput = ({
   const { user } = useUserStore();
   const { updateUserInterviews } = useInterviewStore();
   const [currentText, setCurrentText] = useState("");
+  const form = useForm<AccountFormData>({
+    resolver: zodResolver(interviewValidator),
+    defaultValues: {
+      companyWebsite: "",
+      totalQuestions: 0,
+      type: undefined,
+      level: undefined,
+    },
+  });
   const [extraDetails, setExtraDetails] = useState({
     companyWebsite: "",
     totalQuestions: 0,
@@ -119,122 +146,134 @@ const PositionInput = ({
               <h2 className="text-lg font-bold mb-4 text-slate-900">
                 You are almost there!
               </h2>
-              <div className="w-full py-8">
-                <div className="flex flex-col mb-4 gap-y-1 w-full ">
-                  <label
-                    htmlFor="companyWebsite"
-                    className="text-dark1/60 text-[14px] font-bold"
-                  >
-                    Enter Company website
-                  </label>
-                  <Input
-                    id="companyWebsite"
-                    type="text"
-                    placeholder="Company website"
-                    value={extraDetails.companyWebsite}
-                    onChange={(e) =>
-                      setExtraDetails({
-                        ...extraDetails,
-                        companyWebsite: e.target.value,
-                      })
-                    }
-                    className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-primary1/70 focus:ring-1 focus:ring-primary1/70"
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(handleGenerateInterview)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="companyWebsite"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-dark1/60 text-[14px] font-bold">
+                          Enter Company website or name
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            id="companyWebsite"
+                            type="text"
+                            placeholder="Company website"
+                            value={extraDetails.companyWebsite}
+                            onChange={(e) =>
+                              setExtraDetails({
+                                ...extraDetails,
+                                companyWebsite: e.target.value,
+                              })
+                            }
+                            className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-primary1/70 focus:ring-1 focus:ring-primary1/70"
+                          />
+                        </FormControl>
+                        <FormMessage className="text-[12px]" />
+                      </FormItem>
+                    )}
                   />
-                </div>
-                <div className="flex flex-col mb-4 gap-y-1 w-full ">
-                  <label
-                    htmlFor="companyWebsite"
-                    className="text-dark1/60 text-[14px] font-bold"
-                  >
-                    Total Questions
-                  </label>
-                  <Input
-                    id="totalQuestions"
-                    type="number"
-                    min={1}
-                    max={10}
-                    value={extraDetails.totalQuestions}
-                    placeholder="Total Questions"
-                    onChange={(e) =>
-                      setExtraDetails({
-                        ...extraDetails,
-                        totalQuestions: parseInt(e.target.value),
-                      })
-                    }
-                    className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-primary1/70 focus:ring-1 focus:ring-primary1/70"
-                  />
-                </div>
-                <div className="flex flex-col mb-4 gap-y-1 w-full ">
-                  <label
-                    htmlFor="companyWebsite"
-                    className="text-dark1/60 text-[14px] font-bold"
-                  >
-                    Interview type
-                  </label>
-                  <Select
-                    onValueChange={(value) =>
-                      setExtraDetails({
-                        ...extraDetails,
-                        type: value,
-                      })
-                    }
-                  >
-                    <SelectTrigger className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-dark1/70 focus:ring-1 focus:ring-primary1/20">
-                      <SelectValue placeholder="Select interview type" />
-                    </SelectTrigger>
-                    <SelectContent className="z-[999]">
-                      <SelectGroup>
-                        <SelectLabel>Interview types</SelectLabel>
-                        <SelectItem value="technical">Technical</SelectItem>
-                        <SelectItem value="behavioral">Behavioral</SelectItem>
-                        <SelectItem value="mixed">Mixed</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="flex flex-col mb-4 gap-y-1 w-full ">
-                  <label
-                    htmlFor="companyWebsite"
-                    className="text-dark1/60 text-[14px] font-bold"
-                  >
-                    what is the level of the position?
-                  </label>
-                  <Select
-                    onValueChange={(value) =>
-                      setExtraDetails({
-                        ...extraDetails,
-                        type: value,
-                      })
-                    }
-                  >
-                    <SelectTrigger className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-dark1/70 focus:ring-1 focus:ring-primary1/20">
-                      <SelectValue placeholder="Select interview level" />
-                    </SelectTrigger>
-                    <SelectContent className="z-[999]">
-                      <SelectGroup>
-                        <SelectLabel>Interview level</SelectLabel>
-                        <SelectItem value="senior">Senior</SelectItem>
-                        <SelectItem value="mid">Mid</SelectItem>
-                        <SelectItem value="junior">Junior</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
-              <div className="flex gap-x-4">
-                <AiButton
-                  onPress={handleGenerateInterview}
-                  title="Generate interview"
-                  icon=""
-                  extraClasses="bg-red-500 text-white"
-                />
-                <AiButton
-                  onPress={handleCloseGenerateModal}
-                  title="Cancel"
-                  icon="ion:close"
-                  extraClasses=" bg-dark1/70 text-white"
-                />
-              </div>
+                  <div className="flex flex-col mb-4 gap-y-1 w-full ">
+                    <label
+                      htmlFor="companyWebsite"
+                      className="text-dark1/60 text-[14px] font-bold"
+                    >
+                      Total Questions
+                    </label>
+                    <Input
+                      id="totalQuestions"
+                      type="number"
+                      min={1}
+                      max={10}
+                      value={extraDetails.totalQuestions}
+                      placeholder="Total Questions"
+                      onChange={(e) =>
+                        setExtraDetails({
+                          ...extraDetails,
+                          totalQuestions: parseInt(e.target.value),
+                        })
+                      }
+                      className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-primary1/70 focus:ring-1 focus:ring-primary1/70"
+                    />
+                  </div>
+                  <div className="flex flex-col mb-4 gap-y-1 w-full ">
+                    <label
+                      htmlFor="companyWebsite"
+                      className="text-dark1/60 text-[14px] font-bold"
+                    >
+                      Interview type
+                    </label>
+                    <Select
+                      onValueChange={(value) =>
+                        setExtraDetails({
+                          ...extraDetails,
+                          type: value,
+                        })
+                      }
+                    >
+                      <SelectTrigger className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-dark1/70 focus:ring-1 focus:ring-primary1/20">
+                        <SelectValue placeholder="Select interview type" />
+                      </SelectTrigger>
+                      <SelectContent className="z-[999]">
+                        <SelectGroup>
+                          <SelectLabel>Interview types</SelectLabel>
+                          <SelectItem value="technical">Technical</SelectItem>
+                          <SelectItem value="behavioral">Behavioral</SelectItem>
+                          <SelectItem value="mixed">Mixed</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div className="flex flex-col mb-4 gap-y-1 w-full ">
+                    <label
+                      htmlFor="companyWebsite"
+                      className="text-dark1/60 text-[14px] font-bold"
+                    >
+                      what is the level of the position?
+                    </label>
+                    <Select
+                      onValueChange={(value) =>
+                        setExtraDetails({
+                          ...extraDetails,
+                          level: value,
+                        })
+                      }
+                    >
+                      <SelectTrigger className="border-2 border-dark1/50 rounded-md p-2 pv-0 w-full text-[14px] text-dark1/70 focus:outline-none focus:border-dark1/70 focus:ring-1 focus:ring-primary1/20">
+                        <SelectValue placeholder="Select interview level" />
+                      </SelectTrigger>
+                      <SelectContent className="z-[999]">
+                        <SelectGroup>
+                          <SelectLabel>Interview level</SelectLabel>
+                          <SelectItem value="senior">Senior</SelectItem>
+                          <SelectItem value="mid">Mid</SelectItem>
+                          <SelectItem value="junior">Junior</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div className="flex gap-x-4">
+                    <AiButton
+                      onPress={handleGenerateInterview}
+                      title="Generate interview"
+                      icon=""
+                      extraClasses="bg-red-500 text-white"
+                    />
+                    <div
+                      onClick={handleCloseGenerateModal}
+                      className="flex items-center gap-2 px-4 py-1.5 rounded-full bg-gradient-to-bl from-gray-900 to-black-700 backdrop-blur-md text-white shadow-inner  border border-white/10 text-sm hover:bg-gradient-to-bl hover:from-orange-900 hover:to-orange-950 transition duration-300 ease-in-out cursor-pointer text-[13px] font-medium bg-dark1/70 "
+                    >
+                      Cancel
+                      <Icon icon="ion:close" className="" />
+                    </div>
+                  </div>
+                </form>
+              </Form>
             </div>
           </div>
         </div>
